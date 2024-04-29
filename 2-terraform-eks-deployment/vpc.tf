7f85c44d86d901cfc4ef5393614e01cfd341aeac
Mazzarimovic22 <mattiamazzari@gmail.com>
2024-04-29 18:02:00 +0200

    changed AWS region

--- 2-terraform-eks-deployment/vpc.tf
+++ 2-terraform-eks-deployment/vpc.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "azs" {}
 
 module "my-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
-  version         = "3.19.0"
+  version         = "5.8.1"
   name            = "my-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
